858209fb7199a1dabf4911219327b9f715f9b9ff
Devraj Singh <devrajvit@gmail.com>
2020-02-15 23:09:10 +0530

    minor fix in digital clock

--- simple-digital-clock/clock.js
+++ simple-digital-clock/clock.js
@@ -4,8 +4,17 @@ const tick = () => {
     const now = new Date();
 
     const h = now.getHours();
-    const m = now.getMinutes();
-    const s = now.getSeconds();
+    if (h < 10) {
+        h = '0' + h
+    }
+    let m = now.getMinutes();
+    if (m < 10) {
+        m = '0' + m
+    }
+    let s = now.getSeconds();
+    if (s < 10) {
+        s = '0' + s
+    }
 
     const timeValue = `
     <span>${h}</span> : 
@@ -18,4 +27,6 @@ const tick = () => {
 
 // This is inbuilt function which will execute the function 
 // based on interval provided.
+// setInterval() is function provided by Window object. 
+// It accepts functions and milliseconds as interval
 setInterval(tick, 1000);
