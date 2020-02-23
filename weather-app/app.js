8b53e3574090a3a4fbf364c607aba5b45125d417
Devraj Singh <devrajvit@gmail.com>
2020-02-23 13:54:22 +0530

    added weather app

---
+++ weather-app/app.js
@@ -0,0 +1,51 @@
+const cityForm = document.querySelector('.weather_app');
+// Add eventlistener like enter and then submit etc.
+
+const getCityAndWeather = async (citySearchText) => {
+    console.log('Getting value of ', citySearchText);
+    const cityDetails = await getCityDetails(citySearchText);
+    const weatherDetails = await getWeather(cityDetails.Key);
+
+    console.log(cityDetails);
+    console.log(weatherDetails);
+
+    return {cityDetails, weatherDetails};
+};
+
+const updateUI = (cityDetails, weatherDetails) => {
+    const cityName = cityDetails.EnglishName;
+         const weatherText = weatherDetails.WeatherText; 
+         const temp = weatherDetails.Temperature.Metric.Value;
+         const isDayTime = weatherDetails.IsDayTime;
+         let image = "";
+         if (isDayTime === true) {
+             image = "images/daytime.svg";
+         } else {
+            image = "images/nighttime.svg";
+         }
+    const card = document.querySelector('.weather_info_div');
+    
+    card.innerHTML = `
+    <img src="${image}" alt="weather_mage" class="weather_icon">
+                <p class="weather_city_name">City Name: ${cityName}</p>
+                <p class="weather_cond">Weather Condition: ${weatherText}</p>
+                <p class="weather_temp">Temperature: ${temp} °C</p>
+    `
+};
+cityForm.addEventListener('submit', e => {
+//     // Prevent default action like refresh page etc.
+     e.preventDefault();
+    
+     const cityValueInText = cityForm.city_search.value.trim();
+     console.log("City search text: ", cityValueInText);
+     cityForm.reset();
+    
+     const results = getCityAndWeather(cityValueInText);
+
+     // results is promise here, so fetch the value using then and catch.
+     results.then(data => {
+         updateUI(data.cityDetails, data.weatherDetails); 
+     }).catch(err => {
+         console.log(err);
+     })
+});
---
+++ weather-app/weather.js
@@ -0,0 +1,32 @@
+const key = 'LZ9IsmtgPsA19f6IUEdCR15Aqfi6S7AL';
+
+const getCityDetails = async (cityName) => {
+    const url = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const citySearchText = cityName;
+    const api = `${url}?apikey=${key}&q=${citySearchText}`;
+
+    const response = await fetch(api);
+    const responseData = await response.json();
+
+    // console.log(responseData);
+    return responseData[0];
+
+};
+
+const getWeather = async (cityKey) => {
+    const url = 'http://dataservice.accuweather.com/currentconditions/v1';
+    const completeUrl = `${url}/${cityKey}?apikey=${key}`;
+
+    const response = await fetch(completeUrl);
+
+    const responseData = await response.json();
+    return responseData[0];
+}
+
+// async always returns promise for which we can use then and catch
+// getCityDetails().then(data => {
+//     const cityKey = data.Key;
+//     return getWeather(cityKey); // this will return promise.
+// }).then(data => {
+//     console.log(data)
+// }).catch(err => console.log(err));
